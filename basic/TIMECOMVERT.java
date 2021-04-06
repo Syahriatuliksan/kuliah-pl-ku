67d026d2d18be269a4fa287d8510b8fe686a8492
Syahriatul iksan <80236708+Syahriatuliksan@users.noreply.github.com>
2021-04-06 14:12:40 +0700

    initial commit

---
+++ basic/TIMECOMVERT.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int days = scanner.nextInt();
+
+        //your code goes here
+        int result = days*24*60*60;
+        System.out.println(result);
+    }
+}
